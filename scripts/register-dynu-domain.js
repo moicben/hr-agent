a5dfb468e44f502052e74b8cd01e52ff2f2eb1a6
moicben <moicben@email.com>
2026-03-01 09:25:04 +0800

    up

--- scripts/register-dynu-domain.js
+++
@@ -1,7 +0,0 @@
-// 1.	créer subdomain Dynu, example: mail1.theworkpc.com
-// 2. Resend : Ajouter le domaine
-// 3. Resend : Récupérer DNS records requis
-// 4. Dynu : Injecter ces records
-// 5. Resend : Vérifier domaine -> Boucle de vérification jusqu'à ce que le domaine soit vérifié (10 minutes max, check 30 secondes)
-
-// 100% automatisable.
